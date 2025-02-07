ecddc86bc49d00aefc00df67a1fd2008d4364720
anushkagupta100 <anushkag078@gmail.com>
2025-02-07 09:45:59 +0530

    second work 7/2/25

--- AIML/front/back/file system/file1.js
+++ AIML/front/back/file system/file1.js
@@ -1,6 +1,17 @@
+//const fs = require('fs');
+//const data=fs.readFileSync("./data.txt","utf-8");
+//console.log(data);
+//to string=to convert into string(in the print line)
+//utf-8=to convert into string
+//fs.writeFileSync("./data3.txt","work from another file","utf-8");
+//fs.appendFileSync("./data1.txt","work from another file","utf-8");
+//fs.renameSync("./data3.txt","./data5.txt");
+//fs.unlinkSync("./data5.txt");//unlink used for deletion
 const fs = require('fs');
 const data=fs.readFileSync("./data.txt","utf-8");
 console.log(data);
-//to string=to convert into string(in the print line)
-//utf-8=to convert into string
-
+if(data.match("H")){
+    console.log("H is found");
+    const newdata=data.replace("H","ABES");
+    fs.writeFileSync("./data.txt",newdata,"utf-8");
+}
