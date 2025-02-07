933b7569541f0984ba00e4e7716877452fa18d13
anushkagupta100 <anushkag078@gmail.com>
2025-02-07 09:23:58 +0530

    updated work 7/2/25

---
+++ AIML/front/back/file system/file1.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const data=fs.readFileSync("./data.txt","utf-8");
+console.log(data);
+//to string=to convert into string(in the print line)
+//utf-8=to convert into string
+
